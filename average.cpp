d46e89d12f306e9d92fb94893f99f2db35918395
Pirachach <pirachach_T@cmu.ac.th>
2026-02-26 02:51:37 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
 
+using namespace std;
+
+int main(int argc, char* argv[]){
+    
+    if(argc ==1) {
+        cout<<"Please input numbers to find average.";
+    return 0;
+    }
+    
+    double sum=0.0;
+    int count=0 ;
+    
+    for(int i =1;i<argc;i++){
+        stringstream ss(argv[i]);
+        double num;
+        
+        if(ss >> num){
+            sum += num;
+            count ++;
+        }
+    }
+    
+    double average = sum/count;
+    
+    cout<<"---------------------------------"<< endl;
+    cout<<"Average of "<< count<<" numbers = ";
+    
+    cout<<setprecision(6) << average << endl;
+    cout<<"---------------------------------"<< endl;
+    
+    return 0;
+}
